9b188801008176249e902cf9c75bf534d0fe1cf3
Arjun Anand <aarjun62003@gmail.com>
2023-08-26 12:02:31 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [searchKeyword, setSearchKeyword] = useState('');
+  const [imageLinks, setImageLinks] = useState([]);
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        `https://api.unsplash.com/search/photos?query=${searchKeyword}&per_page=20`,
+        {
+          headers: {
+            Authorization: 'Client-ID tUmP2hndyl8QSsSXoaBUzNury_onveMgQGyr9eDM9Qg',
+          },
+        }
+      );
+
+      const data = await response.json();
+      const links = data.results.map((result) => result.urls.small);
+      setImageLinks(links);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+      <div>
+        <input
+          type="text"
+          placeholder="Search images..."
+          value={searchKeyword}
+          onChange={(e) => setSearchKeyword(e.target.value)}
+        />
+        <button onClick={fetchData}>Search</button>
+      </div>
+      {loading ? (<h1> Loading.... </h1>) : (<div>
+          {imageLinks.map((link, index) => (
+            <img key={index} src={link} alt={`${index}`} />
+          ))}
+        </div>)}
     </div>
   );
 }
